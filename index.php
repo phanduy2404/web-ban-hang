7b2ab38b3ad13a238bf2a8241d2f971d7c35151c
phan duy <phanduy2404@gmail.com>
2020-06-24 19:12:38 +0700

    second-commit

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
             rel="stylesheet"
             href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
         <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="popup.css">
+        <script src="https://kit.fontawesome.com/c0fd8eb209.js" crossorigin="anonymous"></script>
     </head>
     <body>
 
@@ -25,6 +27,7 @@
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+            <a href="#" class="navbar-brand" id="button">Đăng Nhập</a>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
@@ -32,10 +35,10 @@
                             <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="about.php">About</a>
+                        <a class="nav-link" href="#">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
+                        <a class="nav-link" href="shopping-craft.php">Shopping-Craft</a>
                     </li>
                 </ul>
                 <form class="form-inline my-2 my-lg-0">
@@ -56,25 +59,13 @@
             </ul>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="img/hinh1.jpg" alt="" width="1100" height="500">
-                    <div class="carousel-caption">
-                        <h3>Los Angeles</h3>
-                        <p>We had such a great time in LA!</p>
-                    </div>
+                    <img src="img/1.jpg" alt="" width="1100" height="500">
                 </div>
                 <div class="carousel-item">
-                    <img src="img/hinh2.jpg" alt="" width="1100" height="500">
-                    <div class="carousel-caption">
-                        <h3>Chicago</h3>
-                        <p>Thank you, Chicago!</p>
-                    </div>
+                    <img src="img/2.jpg" alt="" width="1100" height="500">
                 </div>
                 <div class="carousel-item">
-                    <img src="img/hinh3.jpg" alt="" width="1100" height="500">
-                    <div class="carousel-caption">
-                        <h3>New York</h3>
-                        <p>We love the Big Apple!</p>
-                    </div>
+                    <img src="img/3.jpg" alt="" width="1100" height="500">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#demo" data-slide="prev">
@@ -93,109 +84,166 @@
                 <div class="row">
                     <div class="col-lg-4 col-md-4 col-12">
                         <div class="card">
-                            <img class="card-img-top" src="img/hinh4.jpg" alt="Card image">
+                            <img class="card-img-top" src="img/1.jpg" alt="Card image">
                             <div class="card-body">
-                                <h4 class="card-title">John Doe</h4>
-                                <p class="card-text">Some example text.</p>
-                                <a href="#" class="btn btn-primary">See Profile</a>
+                                <h4 class="card-title">Áo số 1</h4>
+                                <p class="card-text">Mã: 01</p>
+                                <a href="shopping-craft.php" class="btn btn-primary">Đặt hàng</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-4 col-12">
                         <div class="card">
-                            <img class="card-img-top" src="img/hinh4.jpg" alt="Card image">
+                            <img class="card-img-top" src="img/2.jpg" alt="Card image">
                             <div class="card-body">
-                                <h4 class="card-title">John Doe</h4>
-                                <p class="card-text">Some example text.</p>
-                                <a href="#" class="btn btn-primary">See Profile</a>
+                                <h4 class="card-title">Áo số 2</h4>
+                                <p class="card-text">Mã: 02</p>
+                                <a href="shopping-craft.php" class="btn btn-primary">Đặt hàng</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-4 col-12">
                         <div class="card">
-                            <img class="card-img-top" src="img/hinh4.jpg" alt="Card image">
+                            <img class="card-img-top" src="img/3.jpg" alt="Card image">
                             <div class="card-body">
-                                <h4 class="card-title">John Doe</h4>
-                                <p class="card-text">Some example text.</p>
-                                <a href="#" class="btn btn-primary">See Profile</a>
+                                <h4 class="card-title">Áo số 3</h4>
+                                <p class="card-text">Mã: 03</p>
+                                <a href="shopping-craft.php" class="btn btn-primary">Đặt hàng</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-4 col-12">
                         <div class="card">
-                            <img class="card-img-top" src="img/hinh4.jpg" alt="Card image">
+                            <img class="card-img-top" src="img/4.jpg" alt="Card image">
                             <div class="card-body">
-                                <h4 class="card-title">John Doe</h4>
-                                <p class="card-text">Some example text.</p>
-                                <a href="#" class="btn btn-primary">See Profile</a>
+                                <h4 class="card-title">Áo số 4</h4>
+                                <p class="card-text">Mã: 04</p>
+                                <a href="shopping-craft.php" class="btn btn-primary">Đặt hàng</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-4 col-12">
                         <div class="card">
-                            <img class="card-img-top" src="img/hinh4.jpg" alt="Card image">
+                            <img class="card-img-top" src="img/5.1.jpg" alt="Card image">
                             <div class="card-body">
-                                <h4 class="card-title">John Doe</h4>
-                                <p class="card-text">Some example text.</p>
-                                <a href="#" class="btn btn-primary">See Profile</a>
+                                <h4 class="card-title">Áo số 5</h4>
+                                <p class="card-text">Mã: 05</p>
+                                <a href="shopping-craft.php" class="btn btn-primary">Đặt hàng</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-4 col-12">
                         <div class="card">
-                            <img class="card-img-top" src="img/hinh4.jpg" alt="Card image">
+                            <img class="card-img-top" src="img/6.jpg" alt="Card image">
                             <div class="card-body">
-                                <h4 class="card-title">John Doe</h4>
-                                <p class="card-text">Some example text.</p>
-                                <a href="#" class="btn btn-primary">See Profile</a>
+                                <h4 class="card-title">Áo số 6</h4>
+                                <p class="card-text">Mã: 06</p>
+                                <a href="shopping-craft.php" class="btn btn-primary">Đặt hàng</a>
                             </div>
                         </div>
                     </div>
+                    <div class="col-lg-4 col-md-4 col-12">
+                        <div class="card">
+                            <img class="card-img-top" src="img/7.jpg" alt="Card image">
+                            <div class="card-body">
+                                <h4 class="card-title">Áo số 7</h4>
+                                <p class="card-text">Mã: 07</p>
+                                <a href="shopping-craft.php" class="btn btn-primary">Đặt hàng</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-12">
+                        <div class="card">
+                            <img class="card-img-top" src="img/8.jpg" alt="Card image">
+                            <div class="card-body">
+                                <h4 class="card-title">Áo số 8</h4>
+                                <p class="card-text">Mã: 08</p>
+                                <a href="shopping-craft.php" class="btn btn-primary">Đặt hàng</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-12">
+                        <div class="card">
+                            <img class="card-img-top" src="img/9.jpg" alt="Card image">
+                            <div class="card-body">
+                                <h4 class="card-title">Áo số 9</h4>
+                                <p class="card-text">Mã: 09</p>
+                                <a href="shopping-craft.php" class="btn btn-primary">Đặt hàng</a>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </section>
-        
 
         <section class="my-5">
             <div class="py-5">
                 <h2 class="text-center">Our Service</h2>
             </div>
             <div class="w-50 m-auto">
-            <form action="userinfo.php" method="post">
-                <div class="form-group">
-                    <label>Username</label>
-                    <input type="text" name="user" autocomplete="off" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label>Email</label>
-                    <input type="text" name="email" autocomplete="off" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label>Phone Number</label>
-                    <input type="text" name="phone" autocomplete="off" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label>Comments</label>
-                    <textarea class="form-control"></textarea>
-                </div>
-                <button type="submit" class="btn btn-success">Submit</button>
-            </form>
-        </div>
-        
+                <form action="userinfo.php" method="post">
+                    <div class="form-group">
+                        <label>Username</label>
+                        <input type="text" name="user" autocomplete="off" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Email</label>
+                        <input type="text" name="email" autocomplete="off" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Phone Number</label>
+                        <input type="text" name="phone" autocomplete="off" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Comments</label>
+                        <textarea class="form-control"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-success">Submit</button>
+                </form>
+            </div>
+           
 
         </section>
         <footer>
             <p class="p-3 bg-dark text-white text-center">Thanks for shopping at 1992 shop</p>
+            
+            <div class="popup">
+                <div class="popup-content">
+                    <i
+                        class="fas fa-times"
+                        style="position: absolute ; top: 0px; right: 2px;cursor: pointer;height: 20px;width: 20px;"
+                        id="close"></i>
+                    <input type="text" placeholder="Username">
+                    <input type="password" placeholder="Password">
+                    <a href="index.php" class="button">Đăng nhập</a>
+                </div>
+            </div>
         </footer>
-        
-        
-        
-        
-        
+
+
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script
             src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
         <script
             src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+        <script>
+            document
+                .getElementById("button")
+                .addEventListener("click", function () {
+                    document
+                        .querySelector(".popup")
+                        .style
+                        .display = "flex ";
+                })
+            document
+                .getElementById("close")
+                .addEventListener("click", function () {
+                    document
+                        .querySelector(".popup")
+                        .style
+                        .display = "none";
+                })
+        </script>
     </body>
 </html>
